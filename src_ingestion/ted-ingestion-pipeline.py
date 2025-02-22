1bc6f646f811947634ff9bc862188811c9034d05
ted-jung <jongnag@gmail.com>
2025-02-22 15:31:44 +0900

    Pipeline or ingestion with option for transformation of nodes

--- src_ingestion/ted-ingestion-pipeline.py
+++ src_ingestion/ted-ingestion-pipeline.py
@@ -3,7 +3,7 @@
 # Created: 21, Feb 2025
 # Updated: 22, Feb 2025
 # Writer: Ted, Jung
-# Description: 
+# Description:
 #   Ingestion and Cache
 #   use docker run
 #   > docker run --name redis-vecdb -d -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
@@ -15,7 +15,7 @@ import os
 import re
 import clickhouse_connect
 
-from llama_index.core import Document,Settings
+from llama_index.core import Document, Settings
 from llama_index.core.schema import TransformComponent
 from llama_index.core import SimpleDirectoryReader
 
@@ -36,7 +36,6 @@ Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
 
-
 # Create a client & VectorStore(ClickHouse)
 
 ch_client = clickhouse_connect.get_client(
@@ -47,25 +46,29 @@ ch_client = clickhouse_connect.get_client(
     database="default",
 )
 vector_store = ClickHouseVectorStore(
-    ch_client, 
-    table="quickstart_index",
-    embed_model=embed_model
+    ch_client, table="quickstart_index", embed_model=embed_model
 )
-
 
 
 # Define text_splitter & the textcleaner to be used when transforming In IngestPipeline
 
 text_splitter = TokenTextSplitter(chunk_size=512)
+
+
 class TextCleaner(TransformComponent):
     def __call__(self, nodes, **kwargs):
         modified_nodes = []
         for node in nodes:
             modified_text = re.sub(r"[^0-9A-Za-z ]", "", node.text)
-            new_node = Document(text=modified_text, doc_id=node.doc_id, metadata=node.metadata)
+            new_node = Document(
+                text=modified_text, doc_id=node.doc_id, metadata=node.metadata
+            )
             modified_nodes.append(new_node)
         return modified_nodes
-    
+
+
+curr_dir = os.getcwd()
+documents = SimpleDirectoryReader(f"{curr_dir}/data/paul_graham/").load_data()
 
 
 # Integrates well with IngestPipeline
@@ -76,20 +79,11 @@ ingest_cache = IngestionCache(
 )
 
 pipeline = IngestionPipeline(
-    transformations=[
-        TextCleaner(),
-        text_splitter,
-        embed_model,
-    ],
+    transformations=[TextCleaner(),text_splitter,embed_model],
     vector_store=vector_store,
-    cache=ingest_cache, 
+    cache=ingest_cache,
 )
-
-curr_dir = os.getcwd()
-documents = SimpleDirectoryReader(f"{curr_dir}/data/paul_graham/").load_data()
 nodes = pipeline.run(documents=documents)
-
-
 
 
 index = VectorStoreIndex.from_vector_store(
@@ -98,10 +92,8 @@ index = VectorStoreIndex.from_vector_store(
 )
 
 query_engine = index.as_query_engine()
-
 print(query_engine.query("What did the author do growing up? especially regarding IBM"))
-
-print(100*"=")
+print(100 * "=")
 
 pipeline = IngestionPipeline(
     transformations=[TextCleaner(), text_splitter, embed_model],
