c29305c4425f12f366f243a17b3a9a6b967f1c3b
ted-jung <jongnag@gmail.com>
2025-02-27 11:53:48 +0900

    evaluate queryengine in terms of faithfulness

--- src_evaluate/ted-evaluate-basic.py
+++ src_evaluate/ted-evaluate-basic.py
@@ -1,23 +1,38 @@
+# ===========================================================================
+# Evaluation
+# Date: 30, Jan 2025
+# Updated: 27, Feb 2025
+# Writer: Ted, Jung
+# Description: Evaluate the Query Engine
+#              evaluates whether a response is faithful(i.g, hallucinated?) to the contexts
+# ===========================================================================
+
+
 from llama_index.core import VectorStoreIndex, Settings, SimpleDirectoryReader
-from llama_index.llms.openai import OpenAI
 from llama_index.core.evaluation import FaithfulnessEvaluator, RelevancyEvaluator, RetrieverEvaluator
+
+from llama_index.llms.openai import OpenAI
 from llama_index.llms.ollama import Ollama
+
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
+# Env set for LLM
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-llm = Ollama(model="llama3.2", temperature=0.0, request_timeout=720.0)
-# create llm
-#llm = OpenAI(model="gpt-4", temperature=0.0)
+llm = OpenAI(model="gpt-4o-mini", temperature=0.0)
+# llm = Ollama(model="llama3.2", temperature=0.0, request_timeout=720.0)
 
-# build index
-documents = SimpleDirectoryReader(input_files=["./data/paul_graham/paul_graham_essay_short.txt"]).load_data()
+
+# Build index
+documents = SimpleDirectoryReader(input_files=["./data/paul_graham/paul_graham_essay.txt"]).load_data()
 vector_index = VectorStoreIndex.from_documents(documents=documents)
 
-# define evaluator
+
+# Define FaithfullnewwEvaluator
 evaluator = FaithfulnessEvaluator(llm=llm)
 
-# query index
+
+# Query Index
 # response(response + source)
 query_engine = vector_index.as_query_engine()
 response = query_engine.query(
@@ -28,14 +43,12 @@ print(eval_result)
 print(str(eval_result.passing))
 
 
+# Retriev Index
 retriever = vector_index.as_retriever(similarity_top_k=2)
-
 retriever_evaluator = RetrieverEvaluator.from_metric_names(
     ["mrr", "hit_rate"], retriever=retriever
 )
-
 eval_result2 = retriever_evaluator.evaluate(
     query="query", expected_ids=["node_id1", "node_id2"]
 )
-
 print(eval_result2)
