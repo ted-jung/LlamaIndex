4de304c3f1961fce37af3016c98ee0b022a9e513
ted-jung <jongnag@gmail.com>
2025-03-02 00:17:50 +0900

    multi-modal RAG

---
+++ o-ai/ted-rag-multimodal.py
@@ -0,0 +1,229 @@
+# ===========================================================================
+# RAG - MultiModal
+# Created: 28, Feb 2025
+# Updated: 28, Feb 2025
+# Writer: Ted, Jung
+# Description: 
+#   1. MultiModal
+# ===========================================================================
+
+
+
+import os
+import time
+import requests
+import urllib.request
+import matplotlib.pyplot as plt
+import clickhouse_connect
+
+from pathlib import Path
+from PIL import Image
+
+from llama_index.multi_modal_llms.openai import OpenAIMultiModal
+from llama_index.core.multi_modal_llms.generic_utils import load_image_urls
+from llama_index.core.indices import MultiModalVectorStoreIndex
+
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import SimpleDirectoryReader, StorageContext, Settings
+
+from llama_index.vector_stores.clickhouse import ClickHouseVectorStore
+
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core.storage.storage_context import StorageContext
+
+
+curr_dir = os.getcwd()
+
+
+image_urls = [
+    "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1920/v1640835927/o3pfl41q7m5bj8jardk0.jpg",
+]
+image_documents = load_image_urls(image_urls)
+
+
+
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+Settings.embed_model = embed_model
+openai_mm_llm = OpenAIMultiModal(
+    model="gpt-4o-mini", max_new_tokens=300
+)
+
+
+response = openai_mm_llm.complete(
+    prompt="Describe the images as an alternative text",
+    image_documents=image_documents,
+)
+print(response)
+
+
+
+input_image_path = Path("input_images")
+if not input_image_path.exists():
+    Path.mkdir(input_image_path)
+
+
+
+def plot_images(image_paths):
+    images_shown = 0
+    plt.figure(figsize=(16, 9))
+    for img_path in image_paths:
+        if os.path.isfile(img_path):
+            image = Image.open(img_path)
+
+            plt.subplot(2, 3, images_shown + 1)
+            plt.imshow(image)
+            plt.xticks([])
+            plt.yticks([])
+
+            images_shown += 1
+            if images_shown >= 9:
+                break
+
+
+image_paths = []
+for img_path in os.listdir(f"{curr_dir}/o-ai/input_images"):
+    image_paths.append(str(os.path.join(f"{curr_dir}/o-ai/input_images", img_path)))
+plot_images(image_paths)
+
+
+
+
+
+# put your local directore here
+image_documents = SimpleDirectoryReader(f"{curr_dir}/o-ai/input_images").load_data()
+
+response = openai_mm_llm.complete(
+    prompt="Describe the images as an alternative text",
+    image_documents=image_documents,
+)
+print(response)
+
+
+
+def get_wikipedia_images(title):
+    response = requests.get(
+        "https://en.wikipedia.org/w/api.php",
+        params={
+            "action": "query",
+            "format": "json",
+            "titles": title,
+            "prop": "imageinfo",
+            "iiprop": "url|dimensions|mime",
+            "generator": "images",
+            "gimlimit": "50",
+        },
+    ).json()
+    image_urls = []
+    for page in response["query"]["pages"].values():
+        if page["imageinfo"][0]["url"].endswith(".jpg") or page["imageinfo"][
+            0
+        ]["url"].endswith(".png"):
+            image_urls.append(page["imageinfo"][0]["url"])
+    return image_urls
+
+
+
+
+# Download text and images from Wikipedia
+
+image_uuid = 0
+# image_metadata_dict stores images metadata including image uuid, filename and path
+image_metadata_dict = {}
+MAX_IMAGES_PER_WIKI = 15
+
+wiki_titles = {
+    "Tesla Model Y",
+    "Tesla Model X",
+    # "Tesla Model 3",
+    # "Tesla Model S",
+    # "Kia EV6",
+    # "BMW i3",
+    # "Audi e-tron",
+    # "Ford Mustang",
+    # "Porsche Taycan",
+    # "Rivian",
+    # "Polestar",
+}
+
+data_path = Path(f"{curr_dir}/o-ai/mixed_wiki")
+if not data_path.exists():
+    Path.mkdir(data_path)
+
+for title in wiki_titles:
+    response = requests.get(
+        "https://en.wikipedia.org/w/api.php",
+        params={
+            "action": "query",
+            "format": "json",
+            "titles": title,
+            "prop": "extracts",
+            "explaintext": True,
+        },
+    ).json()
+    page = next(iter(response["query"]["pages"].values()))
+    wiki_text = page["extract"]
+
+    with open(data_path / f"{title}.txt", "w") as fp:
+        fp.write(wiki_text)
+
+    images_per_wiki = 0
+    try:
+        # page_py = wikipedia.page(title)
+        list_img_urls = get_wikipedia_images(title)
+
+        # print(list_img_urls)
+
+        for url in list_img_urls:
+            if (
+                url.endswith(".jpg")
+                or url.endswith(".png")
+                or url.endswith(".svg")
+            ):
+                image_uuid += 1
+                # image_file_name = title + "_" + url.split("/")[-1]
+
+                urllib.request.urlretrieve(
+                    url, data_path / f"{image_uuid}.jpg"
+                )
+                time.sleep(1)
+                images_per_wiki += 1
+                # Limit the number of images downloaded per wiki page to 15
+                if images_per_wiki > MAX_IMAGES_PER_WIKI:
+                    break
+    except Exception as e:
+        print(e)
+        print(
+            "Number of images found for Wikipedia page: {} are {}".format(
+                title, images_per_wiki
+            )
+        )
+        continue
+
+
+
+# Create the MultiModal index
+
+# ClickHouse Vector Store
+ch_client = clickhouse_connect.get_client(
+    host="localhost",
+    port=8123,
+    username="default",
+    cassdord="aa",
+    database="default",
+)
+
+
+# storage_context = StorageContext.vector_stores(vector_store=vector_store)
+vector_store = ClickHouseVectorStore(
+    ch_client, 
+    table="wikipedia_index",
+    embed_model=embed_model
+)
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+documents = SimpleDirectoryReader(f"{curr_dir}/o-ai/mixed_wiki/").load_data()
+index = MultiModalVectorStoreIndex.from_documents(
+    documents,
+    storage_context=storage_context,
+)
+
+storage_context.persist()
