c7cb30e097d745f20af7286a958cbc0c07752162
ted-jung <jongnag@gmail.com>
2025-03-04 14:58:08 +0900

    LLM for multimodal(text, image)

--- o-ai/ted-rag-multimodal.py
+++ o-ai/ted-rag-multimodal.py
@@ -4,13 +4,14 @@
 # Updated: 28, Feb 2025
 # Writer: Ted, Jung
 # Description: 
-#   1. MultiModal
+#   1. MultiModal index & LLM for Multimodal
 # ===========================================================================
 
 
 
 import os
 import time
+from tkinter import image_types
 import requests
 import urllib.request
 import matplotlib.pyplot as plt
@@ -23,18 +24,22 @@ from llama_index.multi_modal_llms.openai import OpenAIMultiModal
 from llama_index.core.multi_modal_llms.generic_utils import load_image_urls
 from llama_index.core.indices import MultiModalVectorStoreIndex
 
-from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.core import SimpleDirectoryReader, StorageContext, Settings
-
 from llama_index.vector_stores.clickhouse import ClickHouseVectorStore
+from llama_index.core import SimpleDirectoryReader, StorageContext, Settings
 
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.storage.storage_context import StorageContext
+
+from llama_index.core import PromptTemplate
+from llama_index.core.query_engine import SimpleMultiModalQueryEngine
+
+from llama_index.core.response.notebook_utils import display_source_node
 
 
 curr_dir = os.getcwd()
 
 
+# Rome colosseum at night
 image_urls = [
     "https://res.cloudinary.com/hello-tickets/image/upload/c_limit,f_auto,q_auto,w_1920/v1640835927/o3pfl41q7m5bj8jardk0.jpg",
 ]
@@ -76,13 +81,30 @@ def plot_images(image_paths):
             plt.yticks([])
 
             images_shown += 1
-            if images_shown >= 9:
+            if images_shown >= 5:
                 break
+
+def plot_images2(image_meta):
+    images_shown = 0
+    plt.figure(figsize=(16, 9))
+    for img_path, img_type in image_meta:
+        if os.path.isfile(img_path) and img_type =="image/jpeg":
+            image = Image.open(img_path)
+
+            plt.subplot(2, 3, images_shown + 1)
+            plt.imshow(image)
+            plt.xticks([])
+            plt.yticks([])
+            images_shown += 1
+            if images_shown >= 5:
+                break
+
 
 
 image_paths = []
 for img_path in os.listdir(f"{curr_dir}/o-ai/input_images"):
     image_paths.append(str(os.path.join(f"{curr_dir}/o-ai/input_images", img_path)))
+
 plot_images(image_paths)
 
 
@@ -129,18 +151,18 @@ def get_wikipedia_images(title):
 image_uuid = 0
 # image_metadata_dict stores images metadata including image uuid, filename and path
 image_metadata_dict = {}
-MAX_IMAGES_PER_WIKI = 15
+MAX_IMAGES_PER_WIKI = 5
 
 wiki_titles = {
     "Tesla Model Y",
     "Tesla Model X",
     # "Tesla Model 3",
     # "Tesla Model S",
-    # "Kia EV6",
+    "Kia EV6",
     # "BMW i3",
     # "Audi e-tron",
     # "Ford Mustang",
-    # "Porsche Taycan",
+    "Porsche Taycan",
     # "Rivian",
     # "Polestar",
 }
@@ -182,6 +204,7 @@ for title in wiki_titles:
                 image_uuid += 1
                 # image_file_name = title + "_" + url.split("/")[-1]
 
+                # download and save it
                 urllib.request.urlretrieve(
                     url, data_path / f"{image_uuid}.jpg"
                 )
@@ -208,22 +231,80 @@ ch_client = clickhouse_connect.get_client(
     host="localhost",
     port=8123,
     username="default",
-    cassdord="aa",
+    password="magic",
     database="default",
 )
 
 
-# storage_context = StorageContext.vector_stores(vector_store=vector_store)
-vector_store = ClickHouseVectorStore(
+# Prepare the target table engine on CH
+text_store = ClickHouseVectorStore(
     ch_client, 
-    table="wikipedia_index",
+    table="text_collection",
     embed_model=embed_model
 )
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+image_store = ClickHouseVectorStore(
+    ch_client, 
+    table="image_collection",
+    embed_model=embed_model
+)
+
+
+# StorageContext having two vector_stores(vector_store=vector_store)
+# Read data & persist it
+storage_context = StorageContext.from_defaults(
+    vector_store=text_store, 
+    image_store=image_store
+)
 documents = SimpleDirectoryReader(f"{curr_dir}/o-ai/mixed_wiki/").load_data()
+storage_context.persist()
+
+
+# Create MM index using two vectorstores
 index = MultiModalVectorStoreIndex.from_documents(
     documents,
     storage_context=storage_context,
 )
 
-storage_context.persist()
+
+
+qa_tmpl_str = (
+    "Context information is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Given the context information and not prior knowledge, respond with both text and relevant images.\n"
+    "If there are images, display them. If there are no images, just return text.\n"
+    "Query: {query_str}\n"
+    "Answer: "
+)
+qa_tmpl = PromptTemplate(qa_tmpl_str)
+
+query_engine = index.as_query_engine(
+    llm=openai_mm_llm, text_qa_template=qa_tmpl
+)
+query_str = "Tell me more about the Porsche Taycan"
+response = query_engine.query(query_str)
+print(f"Response: {response.response}") # print the whole response.
+
+for node in response.source_nodes:
+    print(f"Node Metadata: {node.metadata}")
+
+
+def ted_display_source_node(text_node, source_length=200):
+    """Prints the source node information."""
+    print("Source Node:")
+    print(f"  Node ID: {text_node.node_id}")
+    print(f"  Text: {text_node.get_content()[:source_length]}...")  # Display first 200 chars
+    print(f"**Similarity:** {text_node.score}<br>")
+    print(f"  Metadata: {text_node.metadata}")
+    # Add more details as needed
+
+
+
+for text_node in response.metadata["text_nodes"]:
+    ted_display_source_node(text_node, source_length=200)
+
+
+plot_images2(
+    [(n.metadata["file_path"], n.metadata["file_type"])  for n in response.metadata["text_nodes"]]
+)
