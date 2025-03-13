3d871d64e592ae53949ff40dda503118ca4504bd
ted-jung <jongnag@gmail.com>
2025-03-13 11:50:29 +0900

    handle the concurrent and parallel events

--- src_workflow/ted-workflow-concurrent.py
+++ src_workflow/ted-workflow-concurrent.py
@@ -5,6 +5,7 @@
 # Writer: Ted, Jung
 # Description: 
 #   handle the concurrent and parallel events
+#   events to events in steps
 # ===========================================================================
 
 
@@ -18,7 +19,7 @@ from llama_index.core.workflow import (
     StartEvent, 
     StopEvent, 
     step,
-    Event
+    Event,
 )
 from llama_index.core.response_synthesizers import get_response_synthesizer
 
@@ -35,11 +36,15 @@ class ThirdEvent(Event):
 class FinalEvent(Event):
     query: str
 
+class FinalEvent2(Event):
+    query: str
+
 
 class ParallelFlow(Workflow):
+
     @step
     async def start(self, ctx: Context, ev: StartEvent) -> FirstEvent | SecondEvent | ThirdEvent:
-        ctx.send_event(FirstEvent(query="Query 1"))
+        ctx.send_event(FirstEvent(query="Query 1-1"))
         ctx.send_event(SecondEvent(query="Query 2"))
         ctx.send_event(ThirdEvent(query="Query 3-1"))
         ctx.send_event(ThirdEvent(query="Query 3-2"))
@@ -50,7 +55,11 @@ class ParallelFlow(Workflow):
         print("Running slow query ", ev.query)
         await asyncio.sleep(random.randint(1, 2))
 
-        return FinalEvent(query=ev.query)
+        if ev.query == "Query 1":
+            return FinalEvent(query=ev.query)
+        else:
+            # return None
+            return FinalEvent(query="")
 
 
     @step
@@ -62,21 +71,25 @@ class ParallelFlow(Workflow):
 
 
     @step
-    async def evt_third(self, ctx: Context, ev: ThirdEvent) -> FinalEvent:
+    async def evt_third(self, ctx: Context, ev: ThirdEvent) -> FinalEvent2:
         print("Running slow query ", ev.query)
         await asyncio.sleep(random.randint(2, 3))
 
-        return FinalEvent(query=ev.query)
-    
+        return FinalEvent2(query=ev.query)
+
 
     @step
-    async def synthesize(self, ctx: Context, ev: FinalEvent) -> StopEvent | None:
-        data = ctx.collect_events(ev, [FinalEvent]*4)
-        if data is None:
+    async def synthesize(self, ctx: Context, ev: FinalEvent|FinalEvent2) -> StopEvent | None:
+
+        data = ctx.collect_events(ev, [FinalEvent, FinalEvent, FinalEvent2, FinalEvent2])
+
+        if data is None or len(data) < 4:
+            print("Not all data has been collected")
             return None
 
         first_evt, second_evt, third_evt, third_evt = data
 
+        print("all data has been collected")
         # Convert strings to TextNode objects
         nodes_with_scores = [
             NodeWithScore(node=TextNode(text=first_evt.query)),
@@ -92,7 +105,6 @@ class ParallelFlow(Workflow):
 
         print("Synthesizing results ", ev.query)
         return StopEvent(result=ev.query)
-
 
 
 async def main():
