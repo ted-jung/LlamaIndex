ddba7dede6b57a69c5393a37d5a0f79025020b7e
ted-jung <jongnag@gmail.com>
2025-03-17 11:34:55 +0900

    workflow for corrective RAG with relevancy

--- src_workflow/ted-corrective-rag-workflow.py
+++ src_workflow/ted-corrective-rag-workflow.py
@@ -48,8 +48,8 @@ Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 llm = OpenAI(model="gpt-4o-mini")
 Settings.llm = llm
 
-tavily_ai_api_key ="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"
-# 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
+tavily_ai_api_key ="tvly-xx"
+
 
 
 # define events by actions
