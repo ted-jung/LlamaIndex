a05e294c6db6b220a965d619c3ff668aa2ff0055
ted-jung <jongnag@gmail.com>
2025-04-22 11:49:35 +0900

    AgentWorkflow to create multiple agents

---
+++ src_agent/ted-agent-multiagent-workflow.py
@@ -0,0 +1,179 @@
+# =============================================================================
+# AgentWorkflow to create multiple agents
+# Created: 22, Apr 2025
+# Updated: 22, Apr 2025
+# Writer: Ted, Jung
+# Description: Booking agent(FunctionCallingAgent) with functiontool
+# =============================================================================
+
+import os
+import asyncio
+
+
+from tavily import AsyncTavilyClient
+
+from llama_index.llms.openai import OpenAI
+from llama_index.core.workflow import Context
+
+from llama_index.core.agent.workflow import FunctionAgent, ReActAgent
+from llama_index.core.agent.workflow import AgentWorkflow
+from llama_index.core.agent.workflow import (
+    AgentInput,
+    AgentOutput,
+    ToolCall,
+    ToolCallResult,
+    AgentStream,
+)
+
+
+llm = OpenAI(model="gpt-4o-mini")
+
+
+
+# Four tools(functions) to be used by the agent
+# Store state in the context object
+
+async def search_web(query: str) -> str:
+    """Useful for using the web to answer questions."""
+    client = AsyncTavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+    return str(await client.search(query))
+
+
+async def record_notes(ctx: Context, notes: str, notes_title: str) -> str:
+    """Useful for recording notes on a given topic. Your input should be notes with a title to save the notes under."""
+    current_state = await ctx.get("state")
+    if "research_notes" not in current_state:
+        current_state["research_notes"] = {}
+    current_state["research_notes"][notes_title] = notes
+    await ctx.set("state", current_state)
+    return "Notes recorded."
+
+
+async def write_report(ctx: Context, report_content: str) -> str:
+    """Useful for writing a report on a given topic. Your input should be a markdown formatted report."""
+    current_state = await ctx.get("state")
+    current_state["report_content"] = report_content
+    await ctx.set("state", current_state)
+    return "Report written."
+
+
+async def review_report(ctx: Context, review: str) -> str:
+    """Useful for reviewing a report and providing feedback. Your input should be a review of the report."""
+    current_state = await ctx.get("state")
+    current_state["review"] = review
+    await ctx.set("state", current_state)
+    return "Report reviewed."
+
+
+
+# There are two agents in this workflow
+# FunctionAgent: if supports tool calling
+# ReActAgent: ohterwise
+# Define the FunctionAgent having tools and hand-off control to other agents
+
+research_agent = FunctionAgent(
+    name="ResearchAgent",
+    description="Useful for searching the web for information on a given topic and recording notes on the topic.",
+    system_prompt=(
+        "You are the ResearchAgent that can search the web for information on a given topic and record notes on the topic. "
+        "Once notes are recorded and you are satisfied, you should hand off control to the WriteAgent to write a report on the topic. "
+        "You should have at least some notes on a topic before handing off control to the WriteAgent."
+    ),
+    llm=llm,
+    tools=[search_web, record_notes],
+    can_handoff_to=["WriteAgent"],
+)
+
+write_agent = FunctionAgent(
+    name="WriteAgent",
+    description="Useful for writing a report on a given topic.",
+    system_prompt=(
+        "You are the WriteAgent that can write a report on a given topic. "
+        "Your report should be in a markdown format. The content should be grounded in the research notes. "
+        "Once the report is written, you should get feedback at least once from the ReviewAgent."
+    ),
+    llm=llm,
+    tools=[write_report],
+    can_handoff_to=["ReviewAgent", "ResearchAgent"],
+)
+
+review_agent = FunctionAgent(
+    name="ReviewAgent",
+    description="Useful for reviewing a report and providing feedback.",
+    system_prompt=(
+        "You are the ReviewAgent that can review the write report and provide feedback. "
+        "Your review should either approve the current report or request changes for the WriteAgent to implement. "
+        "If you have feedback that requires changes, you should hand off control to the WriteAgent to implement the changes after submitting the review."
+    ),
+    llm=llm,
+    tools=[review_report],
+    can_handoff_to=["WriteAgent"],
+)
+
+
+
+# Now ready to create the AgentWorkflow
+# The root agent is the one that starts the workflow
+# The initial state is the state of the workflow when it starts
+# run the workflow
+agent_workflow = AgentWorkflow(
+    agents=[research_agent, write_agent, review_agent],
+    root_agent=research_agent.name,
+    initial_state={
+        "research_notes": {},
+        "report_content": "Not written yet.",
+        "review": "Review required.",
+    },
+)
+
+
+
+
+async def main():
+    handler = agent_workflow.run(
+    user_msg=(
+            "Write me a report on the LK99 superconductor."
+            "Briefly describe the LK99, including the development status, the company who announced it, "
+            "and which companies following it. and the potential impact on the semiconductor industry. "
+            "Please provide a summary of the research notes and the report content. "
+        )
+    )
+    
+    current_agent = None
+    current_tool_calls = ""
+    async for event in handler.stream_events():
+        if (
+            hasattr(event, "current_agent_name")
+            and event.current_agent_name != current_agent
+        ):
+            current_agent = event.current_agent_name
+            print(f"\n{'='*50}")
+            print(f"🤖 Agent: {current_agent}")
+            print(f"{'='*50}\n")
+
+        # if isinstance(event, AgentStream):
+        #     if event.delta:
+        #         print(event.delta, end="", flush=True)
+        # elif isinstance(event, AgentInput):
+        #     print("📥 Input:", event.input)
+        elif isinstance(event, AgentOutput):
+            if event.response.content:
+                print("📤 Output:", event.response.content)
+            if event.tool_calls:
+                print(
+                    "🛠️  Planning to use tools:",
+                    [call.tool_name for call in event.tool_calls],
+                )
+        elif isinstance(event, ToolCallResult):
+            print(f"🔧 Tool Result ({event.tool_name}):")
+            print(f"  Arguments: {event.tool_kwargs}")
+            print(f"  Output: {event.tool_output}")
+        elif isinstance(event, ToolCall):
+            print(f"🔨 Calling Tool: {event.tool_name}")
+            print(f"  With arguments: {event.tool_kwargs}")
+
+
+    state = await handler.ctx.get("state")
+    print(state["report_content"])
+
+asyncio.run(main())
