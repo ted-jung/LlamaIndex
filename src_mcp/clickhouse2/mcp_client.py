dd95695d328f3e0c15602ba535c9e5e8869afcfe
ted-jung <jongnag@gmail.com>
2025-05-19 11:31:52 +0900

    using new module in tools.mcp

--- src_mcp/clickhouse2/mcp_client.py
+++ src_mcp/clickhouse2/mcp_client.py
@@ -1,20 +1,22 @@
 # =============================================================================
-# MCP ClickHouse Client
+# MCP Client for ClickHouse
 # Created: 7, Apr 2025
-# Updated: 7, Apr 2025
+# Updated: 19, May 2025
 # Writer: Ted Jung
 # Description:
 #   MCP Client using MCPToolSpec in LlamaIndex to get the list of tool
+#   using new module llama_index.tools.mcp
 # =============================================================================
 
 
 import os
+import sys
 import asyncio
 import logging
-import sys
 
 from platform import system
 from pyexpat import model
+from tabnanny import verbose
 from typing import List, Optional, Dict, Any
 
 from llama_index.core.settings import Settings
@@ -32,10 +34,12 @@ from contextlib import AsyncExitStack
 
 from llama_index.core.llms import ChatMessage
 
+from llama_index.tools.mcp import aget_tools_from_mcp_url
 from mcp.client.session import ClientSession
 from mcp.client.sse import sse_client
 from mcp.client.stdio import stdio_client
 from mcp.client.stdio import StdioServerParameters
+
 
 from mcp_env import ch_config
 
@@ -58,28 +62,28 @@ To help a user, you need to know the tools first. and then answer the user's que
 
 llm = OpenAI(model="gpt-4o-mini", temperature=0.0)
 
-class ClientSession:
-    async def list_tools(self) -> List[Dict]:
-        """
-        Retrieves a list of tools from the MCP server.
-        """
-        raise NotImplementedError("This method must be implemented by a concrete client.")
+# class ClientSession:
+#     async def list_tools(self) -> List[Dict]:
+#         """
+#         Retrieves a list of tools from the MCP server.
+#         """
+#         raise NotImplementedError("This method must be implemented by a concrete client.")
 
-    async def call_tool(self, tool_name: str, input_data: Dict) -> Dict:
-        """
-        Calls a specific tool on the MCP server.
+#     async def call_tool(self, tool_name: str, input_data: Dict) -> Dict:
+#         """
+#         Calls a specific tool on the MCP server.
 
-        Args:
-            tool_name: The name of the tool to call.
-            input_data: A dictionary of arguments for the tool.
+#         Args:
+#             tool_name: The name of the tool to call.
+#             input_data: A dictionary of arguments for the tool.
 
-        Returns:
-            The result of the tool call.
-        """
-        raise NotImplementedError("This method must be implemented by a concrete client.")
+#         Returns:
+#             The result of the tool call.
+#         """
+#         raise NotImplementedError("This method must be implemented by a concrete client.")
 
 
-class MCPClickHouseClient():
+class MCPClient():
     def __init__(self, server_url: str = None):
         self.mcp_server_url = server_url
         self.connected = False
@@ -96,10 +100,17 @@ class MCPClickHouseClient():
             return
 
         try:
-            # Create SSE client
-            mcp_client = BasicMCPClient("http://localhost:8000/sse")
-            self.tool_spec = McpToolSpec(client=mcp_client)
-            tools = await self.tool_spec.to_tool_list_async()
+            # Create SSE client and get list of tools using BasicMCPClient/McpToolSpec
+            # Asynchronous method to convert MCP tools to FunctionTool objects.
+            # return: A list of FuntionTool objects
+            mcp_client = BasicMCPClient(f"http://{self.mcp_server_url}:8000/sse")
+
+            # self.tool_spec = McpToolSpec(client=mcp_client)
+            # tools = await self.tool_spec.to_tool_list_async()
+            tools = await aget_tools_from_mcp_url(
+                f"http://{self.mcp_server_url}:8000/sse",
+                client=mcp_client
+            )
             
             if mcp_client:
                 self.connected = True
@@ -172,12 +183,14 @@ async def main():
     """Example usage of the MCP ClickHouse client"""
     try:
         # Create and connect client
-        mcp_client = MCPClickHouseClient(ch_config.get_client_config()["host"])
+        mcp_client = MCPClient(ch_config.get_client_config()["host"])
         mcp_tools = await mcp_client.connect()
 
         # Create ReActAgent
         agent = await get_react_agent(mcp_tools)
-        #agent = await get_function_agent(mcp_tools)
+
+        # Be able to use FunctionAgent as well
+        # agent = await get_function_agent(mcp_tools)
         # agent_context = Context.from_defaults(agent=agent)
         # agent_context = Context(agent)
 
