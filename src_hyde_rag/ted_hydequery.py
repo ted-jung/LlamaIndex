180fc5de7c9e121f498c97a5131018dc3a133ac4
ted-jung <jongnag@gmail.com>
2025-07-09 16:15:26 +0900

    basic hyde option

--- src_hyde_rag/ted_hydequery.py
+++ src_hyde_rag/ted_hydequery.py
@@ -1,45 +1,62 @@
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
-from llama_index.llms.ollama import Ollama
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_index.core.indices.query.query_transform import HyDEQueryTransform
-from llama_index.core.indices.vector_store import v
 from llama_index.core.query_engine import TransformQueryEngine
+
+from llama_index.llms.ollama import Ollama
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
 from IPython.display import Markdown, display
 
 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
-Settings.llm = Ollama(model="llama3.2", request_timeout=720.0)
+Settings.llm = OpenAI(model="gpt-4.1-nano", request_timeout=720.0)
+#Settings.llm = Ollama(model="llama3.2", request_timeout=720.0)
+
+
 
 # initialize settings (set chunk size)
 Settings.chunk_size = 1024
-# load documents
-documents = SimpleDirectoryReader("./data/paul_graham/").load_data()
 
+
+# load documents and indexing it
+documents = SimpleDirectoryReader("./data/paul_graham/").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
-query_str = "what did paul graham do after going to RISD"
 
+
+# turn index into queryengine, do a query with question
+query_str = "what did paul graham do after going to RISD"
 query_engine = index.as_query_engine()
+
+
+# just a normal query on the query engine
 response = query_engine.query(query_str)
-print("normal vectorstoreindex engine****************")
+print(f"\nnormal vectorstoreindex engine****************")
 print(response)
 
 
+
+# HyDEQueryTransform works by generating a hypothetical document based on the query
+# and using it for embedding lookup
 hyde = HyDEQueryTransform(include_original=True)
 hyde_query_engine = TransformQueryEngine(query_engine, hyde)
 response = hyde_query_engine.query(query_str)
-print("hypotetical document for embedding****************")
+print(f"\nhypotetical document for embedding****************")
 print(response)
+
 
 query_bundle = hyde(query_str)
 hyde_doc = query_bundle.embedding_strs[0]
 
-print("this is the hyde docs****************")
+print(f"\nthis is the hyde docs****************")
 print(hyde_doc)
